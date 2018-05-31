27f2fc1bc3f609b485f85c2ea474ea847ec3d605
GaryVanwynsberghe <vanwynsberghe.gary@gmail.com>
2018-05-31 15:51:40 +0200

    Afficher menu

---
+++ Algo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        private static int GererMenu(List<string> menu)
+        {
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+            }
+
+            int choix;
+            choix = int.Parse(Console.ReadLine());
+            return choix;
+        }
+    }
+}
